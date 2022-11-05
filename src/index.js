a1a6bc3237b245c575af05cb5e3949dab0ee9bb1
KirSivogr <kirillsivograkovv@mail.ru>
2022-11-05 18:13:19 +0300

    add even.js/index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+
+export default function mainLogic (functionName, userName) {
+    let amountOfRightAnswers = 0;
+    while (true) {
+        const [question, rightAnswer] = functionName();
+        console.log(`Question: ${question}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+        if (rightAnswer === userAnswer) {
+            console.log('Correct!');
+            amountOfRightAnswers += 1;
+            if (amountOfRightAnswers === 3) {
+                console.log(`Congratulations, ${userName}!`);
+                break;
+            }
+        }
+        else {
+            console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${userName}!`);
+            break;
+        }
+    }
+}
